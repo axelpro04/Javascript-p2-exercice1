e94a9dd7f3da4ef58b60ab71e65370bb47d1c2fa
axelpro04 <axellefpro@gmail.com>
2021-04-30 11:51:05 +0200

    exo

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('cbd').onmouseover = function onMouseOver() {
+    document.getElementById('cbd').src="assets/img/chew.jpg";
+}
+document.getElementById('cbd').onmouseout = function onMouseOut() {
+    document.getElementById('cbd').src="assets/img/oil.jpg";
+}
